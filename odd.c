281623fe8f70dad90768e778716d10fee776970c
keshav kashyap <168452278+Keshav-Kashyap@users.noreply.github.com>
2024-08-20 23:00:31 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main () {
+  int num;
+printf("Enter The Number:");
+  scanf("%d",&num);
+  if(num%2==0){
+printf("The number is even");
+  }
+else{
+  printf("The number is odd");
+  
+}
+return 0;
+}
